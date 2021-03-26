dcb4159eb5ba2fdbffb2b7992fd9c1b4e9f4e913
SuloForsgren <sulo.forsgren@gmail.com>
2021-03-26 11:29:30 +0200

    html ja javascript 26.3.2021

--- kirjautumisikkuna/koodi.js
+++ kirjautumisikkuna/koodi.js
@@ -1,46 +1,46 @@
 function tarkistus(form) {
 //Määritetään muuttujat ja annetaan niille arvot
-var etunimi = document.getElementById("nimi").value;
+var etunimi = document.getElementById("enimi").value;
 var sähköposti = document.getElementById("sposti").value;
-var salasana = document.getElementById("salasana").value;
+var salainensana = document.getElementById("salasana").value;
 var palauteruutu = document.getElementById("palaute").value;
 var pallukat = document.getElementById("kysymys1").value;
 var checkboksit = document.getElementById("kysymys2").value;
 //Etunimen tarkistus
 if(etunimi.length < 3) {
   alert("Anna vähintään 3 merkkinen etunimi");
-    form.nimi.focus();
+    form.etunimi.focus();
     return false;
   }
   //Sähköpostin tarkistus
 var ehdot = /\S+@\S+/;
-  if(!ehdot.test(sposti))
+  if(!ehdot.test(sähköposti))
   {
     alert("Anna kunnon sähköposti");
-    form.email.focus();
+    form.sähköposti.focus();
     return false;
   }
   //salasanan tarkistus
 if(salasana.length < 5) {
   alert ("Anna kunnollinen salasana");
-  form.salasana.focus();
+  form.salainensana.focus();
   return false;
 }
 //Radio nappien tarkistus
 var tollanen = false;
-for(var i = 0; i < pallukat.length; i++) {
-  if(pallukat[i].checked == true) {
+for(var i = 0; i < kysymys1.length; i++) {
+  if(kysymys1[i].checked == true) {
     tollanen = true;
   }
 }
-if(tollane == false) {
+if(tollanen == false) {
   alert("Et ole valinnut yhtäkään pallukkaa!");
   return false;
 }
 //checkboksien tarkistus
 var checkbox = false;
-for(var j = 0; j < checkboksit.length; j++) {
-  if(checkboksit[j].checked == true) {
+for(var j = 0; j < kysymys2.length; j++) {
+  if(kysymys2[j].checked == true) {
     checkbox = true;
   }
 }
@@ -49,19 +49,21 @@ if(checkbox == false) {
   return false;
 }
 //palautekentän tarkistus
-if(palaute.length < 15) {
+if(palauteruutu.length < 15) {
   alert("Sinun pitää kirjoittaa tähän palautekenttään vähintään 15 kirjainta");
-  form.palaute.focus();
+  form.palauteruutu.focus();
   return false;
-  else {
-    alert("Kiitos kun täytit meidän lomakkeen!")
-  }
+
+if (document.getElementByClass("Send").onclick) {
+
+  alert("Kiitos kun täytit meidän lomakkeen!")
 }
 //Lomakkeen tyhjennys (ehkä jopa toimii)
 function tyhjenna(lomake) {
   var clean = lomake.value;
     if(!confirm("Oletko nyt aivan varma tästä?")) {
       return false;
+      }
     }
   }
 }
